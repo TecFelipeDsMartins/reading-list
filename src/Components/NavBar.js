57038520cd381c55b89846d084907af86c69f2c6
TecFelipeDsMartins <tec.felipedsmartins@gmail.com>
2020-09-18 08:10:44 -0300

    1 file modified

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,10 +1,11 @@
 import React, { useContext } from 'react'
-import { BookContext } from './Context/BookContext'
+import { BookContext } from '../Context/BookContext'
+
 const NavBar = () => {
   const {books} = useContext(BookContext)
   return(
     <div>
-      <p>{books.length}</p>
+      <p>{books}</p>
     </div>
   )
 }
