c3a67a8eb4347a11f7ef3b2de3f9c083e57906a6
TecFelipeDsMartins <tec.felipedsmartins@gmail.com>
2020-09-19 02:43:37 -0300

    3 files modified

---
+++ src/Components/BookList.js
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react'
+import { BookContext } from '../Context/BookContext'
+
+const BookList = () => {
+    const {books} = useContext(BookContext)
+  return books.length ? (
+    <div className="book-list">
+      <ol>
+        {books.map( book => {
+          return(<BookDetails book={book} key={book.id}/>)
+          }
+        )}
+      </ol>
+    </div>
+  ) : (
+    <div className='empty'>Não existe livos para ler</div>
+  )
+}
+
+export default BookList
